399e5a642d58acded772519d4b847fe2af87ce36
vishalag53 <vishal.agrawal1207@gmail.com>
2025-01-14 15:56:12 +0530

    App Icon and Splash Screen

--- app/src/main/java/com/vishalag53/offlinemusic/MainActivity.kt
+++ app/src/main/java/com/vishalag53/offlinemusic/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setTheme(R.style.Theme_OfflineMusic)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
